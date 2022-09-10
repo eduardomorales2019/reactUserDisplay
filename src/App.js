3b029ea32c39bd3a9dba43449ced0ad82a8f5a58
EduardoMorales <eduardo@inmo-puebla.com>
2022-09-10 13:41:27 -0500

    se añaden los estados para la funcion de la aplicacion

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import "./index.css";
+import React, { useState, useEffect } from "react";
+
+// We have to create States for Proyect.
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [people, setpeople] = useState(null);
+  const [title, setTitle] = useState("name");
+
+  const [value, setValue] = useState("random Person");
+
   return (
     <div>
       <h1>Person display App </h1>
